ce947d5b74a942d3e686780625f31a5e6dd89c68
acatala2 <tumthe3@gmail.com>
2018-02-17 13:23:11 -0500

    Add Weapon properties and constructor

--- scripts/objects/weapon.ts
+++ scripts/objects/weapon.ts
@@ -1,5 +1,11 @@
 module objects {
     export class Weapon {
-        
+        public fireRate: number;
+        public projectile: Projectile;
+
+        constructor(fireRate:number, projectile:Projectile) {
+            this.fireRate = fireRate;
+            this.projectile = projectile;
+        }
     }
 }
