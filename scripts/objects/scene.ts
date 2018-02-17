b2c2be40dac46377a4c2939f81e59350decd3f05
acatala2 <tumthe3@gmail.com>
2018-02-17 13:43:30 -0500

    Add first scene

--- scripts/objects/scene.ts
+++ scripts/objects/scene.ts
@@ -1,15 +1,16 @@
 module objects {
-    export abstract class Scene {
-        public Start():void {
+    export abstract class Scene extends createjs.Container {
+        public assetManager;
 
+        constructor(assetManager: createjs.LoadQueue) {
+            super();
+            this.assetManager = assetManager;
         }
+    
+        public Start():void { }
 
-        public Update():void {
+        public Update():void { }
 
-        }
-
-        public Main():void {
-            
-        }
+        public Main():void { }
     }
 }
---
+++ scripts/scenes/start.ts
@@ -0,0 +1,20 @@
+module scenes {
+    export class StartScene extends objects.Scene {
+        constructor(assetManager: createjs.LoadQueue) {
+            super(assetManager);
+            this.Start();
+        }
+
+        public Start():void {
+
+        }
+
+        public Update():void {
+
+        }
+
+        public Main():void {
+            
+        }
+    }
+}
