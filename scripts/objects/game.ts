d97956d04244f96b1264c0d21ebacd350476fffc
LeicaSimile <leicasimile@gmail.com>
2018-02-23 11:53:49 -0500

    Add map boundary variables

--- scripts/objects/game.ts
+++ scripts/objects/game.ts
@@ -7,5 +7,11 @@ module objects {
         public static readonly WIDTH = 640;
         public static readonly HEIGHT = 480;
         public static readonly FPS = 60;
+
+        // Points that the player has to reach to start moving around the map
+        public static readonly TOP_BOUNDARY = 200; // move up
+        public static readonly BOTTOM_BOUNDARY = Game.HEIGHT - 20; // move down
+        public static readonly LEFT_BOUNDARY = 10; // move left
+        public static readonly RIGHT_BOUNDARY = Game.WIDTH - 10; // move right
     }
 }
