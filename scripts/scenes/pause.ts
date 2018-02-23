3d5273989995a82fd170734b951b667ba3997210
LeicaSimile <leicasimile@gmail.com>
2018-02-23 11:22:24 -0500

    Add pause scene

---
+++ scripts/scenes/pause.ts
@@ -0,0 +1,20 @@
+module scenes {
+    export class PauseScene extends objects.Scene {
+        constructor(assetManager: createjs.LoadQueue) {
+            super(assetManager);
+            this.Start();
+        }
+
+        public Start():void {
+
+        }
+
+        public Update():number {
+            return 0;
+        }
+
+        public Main():void {
+            
+        }
+    }
+}
