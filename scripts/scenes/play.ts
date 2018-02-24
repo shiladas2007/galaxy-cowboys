cd9c242e7ca0121d096c83df362f3265f47e4a02
acatala2 <tumthe3@gmail.com>
2018-02-24 13:55:38 -0500

    Remove redundant collision detection function

--- scripts/scenes/play.ts
+++ scripts/scenes/play.ts
@@ -5,16 +5,6 @@ module scenes {
             this.Start();
         }
 
-        protected _checkCollision(a:objects.GameObject, b:objects.GameObject):void {
-            let pointA: createjs.Point = a.position;
-            let pointB: createjs.Point = b.position;
-
-            if ((Math.sqrt(Math.pow(pointB.x - pointA.x, 2) + Math.pow(pointB.y - pointA.y, 2))) <
-            (a.halfHeight + b.halfHeight)) {
-                // collision
-            }
-        }
-
         public Start():void {
 
         }
