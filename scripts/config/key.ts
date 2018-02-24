1403c6bedfe48610460f64f3be819d88a8c5452d
acatala2 <tumthe3@gmail.com>
2018-02-24 14:31:59 -0500

    Add keybindings

---
+++ scripts/config/key.ts
@@ -0,0 +1,8 @@
+module config {
+    export class Key {
+        public static readonly UP:string[] = ["w"];
+        public static readonly DOWN:string[] = ["s"];
+        public static readonly LEFT:string[] = ["a"];
+        public static readonly RIGHT:string[] = ["d"];
+    }
+}
