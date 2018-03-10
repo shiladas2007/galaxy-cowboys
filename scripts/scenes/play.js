43bf7a4977992282436f60bc55b690ecb5d2c112
LeicaSimile <leicasimile@gmail.com>
2018-03-10 12:42:28 -0500

    Add transpiled scripts

--- scripts/scenes/play.js
+++ scripts/scenes/play.js
@@ -22,17 +22,17 @@ var scenes;
         PlayScene.prototype.update = function () {
             var _this = this;
             // TODO: centralize collision check
-            var k = true;
+            var hasCollided = false;
             this._map.update();
             this._player.update();
             // Check for collisions
             this._enemies.forEach(function (enemy) {
                 enemy.update();
                 if (managers.Collision.check(_this._player, enemy)) {
-                    k = true;
+                    hasCollided = true;
                 }
             });
-            if (k) {
+            if (!hasCollided) {
                 this._player.lastValidPosition.x = this._player.x;
                 this._player.lastValidPosition.y = this._player.y;
             }
