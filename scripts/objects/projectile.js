65b9bde060a355c8b2db7ed6471d2f29e4511d3b
LeicaSimile <leicasimile@gmail.com>
2018-03-11 19:15:18 -0400

    Add function for moving projectile

--- scripts/objects/projectile.js
+++ scripts/objects/projectile.js
@@ -12,9 +12,36 @@ var objects;
 (function (objects) {
     var Projectile = /** @class */ (function (_super) {
         __extends(Projectile, _super);
-        function Projectile(imageName, px, py) {
-            return _super.call(this, imageName, px, py) || this;
+        function Projectile(imageName, origin, destination, mvspd) {
+            if (mvspd === void 0) { mvspd = 1; }
+            var _this = _super.call(this, imageName, origin.x, origin.y) || this;
+            _this._origin = origin;
+            _this._destination = destination;
+            _this.mvspd = mvspd;
+            return _this;
         }
+        Object.defineProperty(Projectile.prototype, "mvspd", {
+            get: function () {
+                return this._mvspd;
+            },
+            set: function (mvspd) {
+                this._mvspd = mvspd;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Projectile.prototype.getNextPosition = function () {
+            var newX;
+            var newY;
+            newX = this.x + (math.Vec2.run(this._origin, this._destination) * this.mvspd);
+            newY = this.y + (math.Vec2.rise(this._origin, this._destination) * this.mvspd);
+            return new math.Vec2(newX, newY);
+        };
+        Projectile.prototype.move = function () {
+            var newPosition = this.getNextPosition();
+            this.x = newPosition.x;
+            this.y = newPosition.y;
+        };
         return Projectile;
     }(objects.GameObject));
     objects.Projectile = Projectile;
--- scripts/objects/projectile.ts
+++ scripts/objects/projectile.ts
@@ -1,9 +1,37 @@
 module objects {
     export class Projectile extends objects.GameObject {
-        private shooter: animate.Animate;
+        private _origin: math.Vec2;
+        private _destination: math.Vec2;
+        private _mvspd: number; // multiplier; 1 = 100%
 
-        constructor(imageName:string, px:number, py:number) {
-            super(imageName, px, py);
+        get mvspd():number {
+            return this._mvspd;
+        }
+        set mvspd(mvspd:number) {
+            this._mvspd = mvspd;
+        }
+
+        constructor(imageName:string, origin:math.Vec2, destination:math.Vec2, mvspd:number=1) {
+            super(imageName, origin.x, origin.y);
+            this._origin = origin;
+            this._destination = destination;
+            this.mvspd = mvspd;
+        }
+
+        private getNextPosition():math.Vec2 {
+            let newX: number;
+            let newY: number;
+            
+            newX = this.x + (math.Vec2.run(this._origin, this._destination) * this.mvspd);
+            newY = this.y + (math.Vec2.rise(this._origin, this._destination) * this.mvspd);
+
+            return new math.Vec2(newX, newY);
+        }
+
+        public move() {
+            let newPosition: math.Vec2 = this.getNextPosition();
+            this.x = newPosition.x;
+            this.y = newPosition.y;
         }
     }
 }
