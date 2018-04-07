dedd0c041ccc0c22fd4053cc2569c82537116530
LeicaSimile <leicasimile@gmail.com>
2018-04-07 13:20:25 -0400

    Add boundary properties for scenes

--- scripts/objects/scene.ts
+++ scripts/objects/scene.ts
@@ -2,6 +2,19 @@ module objects {
     export abstract class Scene extends createjs.Container {
         public assetManager;
 
+        get topBoundary(): number {
+            return this.y;
+        }
+        get bottomBoundary(): number {
+            return this.y + managers.Game.HEIGHT;
+        }
+        get leftBoundary(): number {
+            return this.x;
+        }
+        get rightBoundary(): number {
+            return this.x + managers.Game.WIDTH;
+        }
+
         constructor() {
             super();
             this.assetManager = managers.Game.assetManager;
