a27bc7b7f31727f05534801cc393cc533b4a7bb0
LeicaSimile <leicasimile@gmail.com>
2018-04-11 13:49:13 -0400

    Get rid of other redundant js files

--- scripts/config/character.js
+++
@@ -1,9 +0,0 @@
-var config;
-(function (config) {
-    var Character;
-    (function (Character) {
-        Character[Character["GUNSLINGER"] = 0] = "GUNSLINGER";
-        Character[Character["QUICKSILVER"] = 1] = "QUICKSILVER";
-    })(Character = config.Character || (config.Character = {}));
-})(config || (config = {}));
-//# sourceMappingURL=character.js.map
